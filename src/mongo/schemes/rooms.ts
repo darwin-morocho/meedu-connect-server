5a36d52c2fd28c9fc6355f2822a0cd9e9bd47ae2
Darwin Morocho <darwin@MacBook-Pro-de-Darwin.local>
2020-04-26 09:30:49 -0500

    added now.json

--- src/mongo/schemes/rooms.ts
+++ src/mongo/schemes/rooms.ts
@@ -1,14 +1,61 @@
 import { Schema, Document, model } from 'mongoose';
 
+export interface UserConnection {
+  socketId: string;
+  username: string;
+  createdAt?: string;
+}
 export interface Room extends Document {
   name: string;
+  connections: UserConnection[];
 }
 
+// schema
 const schema = new Schema(
   {
     name: { type: String, required: true, unique: true },
+    connections: {
+      type: [{ socketId: String, username: String, createdAt: Date.now() }],
+      default: [],
+    },
   },
   { timestamps: true }
 );
 
-export default model<Room>('room', schema);
+// model
+const Rooms = model<Room>('room', schema);
+
+export const checkRoom = async (name: string): Promise<Room | null> => {
+  try {
+    const room = await Rooms.findOne({ name });
+    return room;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+const connections = new Map<string, UserConnection[]>();
+
+/**
+ * check if a room exist and return the room with the current connected users
+ * @param roomName
+ * @param userConnection
+ */
+export const addConnection = async (
+  roomName: string,
+  userConnection: UserConnection
+): Promise<Room | null> => {
+  const room = await checkRoom(roomName);
+  if (!room) return room;
+  const tmp = room.toJSON();
+  room.connections.push(userConnection);
+  await room.save();
+  return tmp;
+};
+
+export const removeConnection = async (roomName: string, socketId: string): Promise<void> => {
+  await Rooms.updateOne({ name: roomName }, { $pull: { connections: { socketId } } });
+};
+
+export default Rooms;
--- src/ws/index.ts
+++ src/ws/index.ts
@@ -3,32 +3,7 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-console */
 import { Server as IOServer, Socket } from 'socket.io';
-
-interface UserConnection {
-  socketId: string;
-  username: string;
-}
-
-const connections = new Map<string, UserConnection[]>();
-
-const addConnection = (roomName: string, userConnection: UserConnection): void => {
-  if (!connections.has(roomName)) {
-    connections.set(roomName, [userConnection]);
-  } else {
-    const tmp: UserConnection[] = connections.get(roomName)!;
-    connections.set(roomName, tmp.concat([userConnection]));
-  }
-};
-
-const removeConnection = (roomName: string, socketId: string): void => {
-  if (connections.has(roomName)) {
-    const tmp: UserConnection[] = connections.get(roomName)!;
-    const index = tmp.findIndex((item) => item.socketId === socketId);
-    if (index !== -1) {
-      connections.set(roomName, tmp.splice(index, 1));
-    }
-  }
-};
+import Rooms, { addConnection, removeConnection } from '../mongo/schemes/rooms';
 
 const ws = (io: IOServer): void => {
   io.on('connection', (socket: Socket) => {
@@ -41,45 +16,51 @@ const ws = (io: IOServer): void => {
 
     socket.on('join-to', async (roomName: string) => {
       if (!socket.rooms[roomName]) {
-        // if the user is not yet in the room
-        const connectedUsers = connections.get(roomName) || [];
-        console.log('connectedUsers', connectedUsers);
-        socket.join(roomName);
         const userConnection = { socketId: socket.id, username };
-        addConnection(roomName, userConnection);
-        socket.handshake.query.room = roomName;
-        socket.broadcast.to(roomName).emit('joined', userConnection); // notify to the others users
-        io.to(socket.id).emit('joined-to', { roomName, connectedUsers }); // notify to the user
+        const room = await addConnection(roomName, userConnection);
+        if (room) {
+          socket.join(roomName);
+          // if the user is not yet in the room
+          const connectedUsers = room.connections;
+          console.log('connectedUsers', connectedUsers);
 
-        // new ice canditate was recived
-        socket.on('new-ice-candidate', (data: { socketId: string; candidate: any }) => {
-          // send the ice cantidate to the user
-          io.to(data.socketId).emit('ice-canditate', {
-            socketId: socket.id,
-            candidate: data.candidate,
-          });
-        });
+          addConnection(roomName, userConnection);
+          socket.handshake.query.room = roomName;
+          socket.broadcast.to(roomName).emit('joined', userConnection); // notify to the others users
+          io.to(socket.id).emit('joined-to', { roomName, connectedUsers }); // notify to the user
 
-        // new offer was recived
-        socket.on('offer', (data: { socketId: string; offer: any }) => {
-          // send the offer to the user
-          io.to(data.socketId).emit('offer', {
-            socketId: socket.id,
-            offer: data.offer,
+          // new ice canditate was recived
+          socket.on('new-ice-candidate', (data: { socketId: string; candidate: any }) => {
+            // send the ice cantidate to the user
+            io.to(data.socketId).emit('ice-canditate', {
+              socketId: socket.id,
+              candidate: data.candidate,
+            });
           });
-        });
 
-        // new answer was recived
-        socket.on('answer', (data: { socketId: string; answer: any }) => {
-          // send the answer to the user
-          io.to(data.socketId).emit('answer', {
-            socketId: socket.id,
-            answer: data.answer,
+          // new offer was recived
+          socket.on('offer', (data: { socketId: string; offer: any }) => {
+            // send the offer to the user
+            io.to(data.socketId).emit('offer', {
+              socketId: socket.id,
+              offer: data.offer,
+            });
           });
-        });
-        socket.on('leave', () => {
-          socket.broadcast.to(roomName).emit('disconnected-user', socket.id);
-        });
+
+          // new answer was recived
+          socket.on('answer', (data: { socketId: string; answer: any }) => {
+            // send the answer to the user
+            io.to(data.socketId).emit('answer', {
+              socketId: socket.id,
+              answer: data.answer,
+            });
+          });
+          socket.on('leave', () => {
+            socket.broadcast.to(roomName).emit('disconnected-user', socket.id);
+          });
+        } else {
+          io.to(socket.id).emit('room-not-found', roomName);
+        }
       }
     });
 
