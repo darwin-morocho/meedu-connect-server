ad94360497b02449d0ce2a8d0f45406430a18952
Darwin Morocho <darwin@MacBook-Pro-de-Darwin.local>
2020-05-13 14:45:26 -0500

    adde cors

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 v1(app);
 const server = http.createServer(app);
 
